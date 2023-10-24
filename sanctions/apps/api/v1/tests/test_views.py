87f55c9bc406f1592b49bae8e1e4946c88f5725d
julianajlk <julianajlk@gmail.com>
2023-10-24 17:50:50 -0400

    test: Add testing factory and SanctionsCheckView tests

---
+++ sanctions/apps/api/v1/tests/test_views.py
@@ -0,0 +1,48 @@
+"""
+Tests for Sanctions API v1 views.
+"""
+import json
+from unittest import mock
+
+from rest_framework.reverse import reverse
+
+from test_utils import APITest
+
+
+class TestSanctionsCheckView(APITest):
+    """ Test SanctionsCheckView. """
+
+    def setUp(self):
+        super().setUp()
+        self.url = reverse('api:v1:sanctions-check')
+        self.post_data = {
+            'lms_user_id': self.user.lms_user_id,
+            'full_name': 'Din Grogu',
+            'city': 'Jedi Temple',
+            'country': 'SW',
+        }
+        self.token = self.generate_jwt_token_header(self.user)
+
+    def test_sdn_check_missing_args(self):
+        response = self.client.post(self.url, HTTP_AUTHORIZATION=self.token)
+        assert response.status_code == 400
+
+    # TODO: add test for test_sdn_check_search_fails_uses_fallback
+
+    @mock.patch('sanctions.apps.sanctions.utils.SanctionsClient.search')
+    def test_sdn_check_search_succeeds(
+        self,
+        mock_search
+    ):
+        mock_search.return_value = {'total': 4}
+        self.set_jwt_cookie(self.user.id)
+        response = self.client.post(
+            self.url,
+            content_type='application/json',
+            HTTP_AUTHORIZATION=self.token,
+            data=json.dumps(self.post_data)
+        )
+        assert response.status_code == 200
+        assert response.json()['hit_count'] == 4
+        assert response.json()['sdn_response'] == {'total': 4}
+        # TODO: add mock_fallback.assert_not_called()
---
+++ sanctions/apps/core/tests/factories.py
@@ -0,0 +1,33 @@
+"""
+Factoryboy factories.
+"""
+
+import logging
+
+import factory
+from faker import Faker
+
+from sanctions.apps.core.models import User
+
+USER_PASSWORD = 'password'
+
+FAKER = Faker()
+
+# Silence faker locale warnings
+logging.getLogger("faker").setLevel(logging.ERROR)
+
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """
+    Test factory for the `User` model.
+    """
+    id = factory.Sequence(lambda n: n + 1)
+    lms_user_id = factory.Sequence(lambda n: n + 1)
+    username = factory.Faker('user_name')
+    password = factory.PostGenerationMethodCall('set_password', USER_PASSWORD)
+    email = factory.Faker('email')
+    is_staff = False
+    is_superuser = False
+
+    class Meta:
+        model = User
