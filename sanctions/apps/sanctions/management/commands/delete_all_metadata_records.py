6c0bc2dedcc401ff0c334072199b51c08e706c46
Chris Pappas <cpappas@edx.org>
2023-11-20 12:09:17 -0500

    temp: add management command to delete all test records

---
+++ sanctions/apps/sanctions/management/commands/delete_all_metadata_records.py
@@ -0,0 +1,28 @@
+"""
+Django management command to delete all metadata records.
+"""
+import logging
+
+from django.core.management.base import BaseCommand
+
+from sanctions.apps.sanctions.models import SanctionsCheckFailure
+
+logger = logging.getLogger(__name__)
+
+
+class Command(BaseCommand):
+    """
+    Command to delete all SanctionsCheckFailure records.
+    """
+    help = 'Delete all SanctionsCheckFailure records.'
+
+    def handle(self, *args, **options):
+        logger.info('Beginning deletion of SanctionsCheckFailure records.')
+        all_records = SanctionsCheckFailure.objects.all().iterator()
+
+        for record in all_records:
+            logger.info('Deleting record %s', record.id)
+            record.delete()
+            logger.info('Deleted record %s', record.id)
+        
+        logger.info('Completed deletion of SanctionsCheckFailure records.')
